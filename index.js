20c18556e897363d36a426dda2af7a14de54ed4d
RedAntDroid <dislevelingup@gmail.com>
2025-01-29 12:01:54 +0530

    ss

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-require('dotenv').config();
 const express = require('express');
 const { resolve } = require('path');
 
@@ -7,24 +5,9 @@ const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-const isAdmin = process.env.IS_ADMIN === 'true';
-
-if (isAdmin) {
-  console.log("Admin privileges granted.");
-} else {
-  console.log("Access restricted. Admin only.");
-}
-
-
+// const app = express();
 
 app.get('/', (req, res) => {
   if (process.env.IS_ADMIN === 'true') {
@@ -33,5 +16,6 @@ app.get('/', (req, res) => {
     res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
   }
 });
-
-app.listen(3000, () => console.log("Server is running on port 3000."));
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
